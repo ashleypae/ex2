263411ef9d34a1462e59f71f72f6df53b4bdc47a
Ashley Pae <aypae@ucdavis.edu>
2016-01-19 15:14:37 -0800

    First commit

---
+++ js/madlibs.js
@@ -0,0 +1,79 @@
+window.onload = function() {
+  //check that js is working
+  console.log("reading js");
+
+  //capture the submit event
+  document.f.onsubmit = processForm;
+  
+  //capture the reset event
+  document.f.onreset=resetPage;
+
+  //define process function
+  function processForm() {
+
+    //store name in a variable
+    var name = document.f.name.value;
+
+    var where = document.f.where.value;
+      
+    var why = document.f.why.value;
+      
+    var howget = document.f.howget.value;
+      
+    var howlong = document.f.howlong.value;
+
+    //error detection
+    if (name == "" || where == "" || why == "" || howget == "" || howlong == "") {
+      alert("you forgot something..");
+
+    } else {
+
+       //play song
+        var audio = new Audio('nsync.mp3');
+audio.play();
+        
+      //capture the msg element to change it's text and html    
+      var myMsg = document.getElementById("myMsg");
+
+   myMsg.innerHTML = "My dearest <em> " + name + "</em>, I know you <em> " + howget + "</em> to <em> " + where + "</em> because you said, ''<em> " + why + ".</em>'' I asked when you're coming back... You looked at me and said, ''<em> " + howlong + ".</em>'' It really hurts and bby I just want you back. <br> xoxo love justin";
+      myMsg.className = "show";
+    }
+  
+    //prevent page from reloading
+    return false;
+  }
+  
+//if user chooses "reset"
+  function resetPage(){
+    //remove any text from myMsg
+    myMsg.innerHTML = "";   
+    //change the class name
+    myMsg.className = "hide";
+
+      
+      
+    //reset the audio  
+sound.pause();
+sound.currentTime = 0;   
+      
+      
+      
+      
+    //reset the name field
+    name.value = "";
+    //reset the where field
+    where.value = "";
+    //reset the why field
+    why.value = "";  
+    //reset the howget field
+    howget.value = "";
+    //reset the howlong field
+    howlong.value = "";
+      
+    
+    return false;
+      
+      
+    
+  }
+}
